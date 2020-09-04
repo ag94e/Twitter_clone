7558c319e8061f3434dc4cea7d8a4f0c13cb60af
ag94e <a.guerrero.esp94@gmail.com>
2020-09-04 00:00:40 -0500

    init the proyect and make app users

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Twitter_clone
-A project for make a clone of twitter.
+
+### This repo was created to make the challenge 🔥 #100DaysOfCode.
+    - Here will put the log of the projects I make.
+        - [R1](./R1.md)
+
+I will update this readme as I see the info must be updated.
+
+## Dependencies
+Python 3
+Django 3.1.1
+
+## License
+MIT License
+
+## Author.
+Antonio Guerrero.
---
+++ twitter_clone/users/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.dispatch import receiver
+from django.core.validators import RegexValidator
+
+class Profile(models.Model):
+    """ Profile model.
+    A proxy model that extends the data
+    with data with extra information.
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_regex = RegexValidator(
+        regex = r'\+?1?\d{9,15}$',
+        message = "Phone number must be entered in th format: +999999999999. Up to 15 digits allowed."
+    )
+    phone_number = models.CharField(
+        validators=[phone_regex],
+        max_length=15,
+        blank=True
+    )
+    picture = models.ImageField(
+        upload_to='users/pictures',
+        blank=True,
+        null=True
+    )
+    about_me = models.TextField(max_length=250, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return username."""
+        return self.user.username
